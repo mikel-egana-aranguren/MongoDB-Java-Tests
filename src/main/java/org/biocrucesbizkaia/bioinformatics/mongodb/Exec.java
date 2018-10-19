3096ed63e08f01d842c231fc562a10846ae22d00
Mikel Egaña Aranguren <mikel.egana.aranguren@gmail.com>
2018-10-19 13:27:13 +0200

    Delete documents

--- src/main/java/org/biocrucesbizkaia/bioinformatics/mongodb/Exec.java
+++ src/main/java/org/biocrucesbizkaia/bioinformatics/mongodb/Exec.java
@@ -4,31 +4,30 @@ import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+
 import java.net.UnknownHostException;
+import org.bson.Document;
 
 public class Exec {
 
 	public static void main(String[] args) {
-		try {
-			MongoClient mongoClient = new MongoClient();
-			DB db = mongoClient.getDB( "JavaTests" );
+		MongoClient mongoClient = new MongoClient();
+		//DB db = mongoClient.getDB( "JavaTests" );
 //			System.out.println("JavaCollection exists: " + db.collectionExists("JavaCollection"));
 //			DBCollection coll = db.getCollection("JavaCollection");
-			DBCollection coll = db.getCollection("BigCollection");
-			
-			int i = 0;
-			
-			while (i < 75000) {
-				i+=1;
-				System.out.println(i);
-				BasicDBObject doc = new BasicDBObject("name", "MongoDB")
-				        .append("type", "database")
-				        .append("count", i)
-				        .append("info", new BasicDBObject("x", 203).append("y", 102));
-				coll.insert(doc);
-			}
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
-		}
+		MongoCollection coll = mongoClient.getDatabase("JavaTests").getCollection("BigCollection");
+		
+//		int i = 0;
+//		while (i < 75000) {
+//			i+=1;
+//			System.out.println(i);
+//			BasicDBObject doc = new BasicDBObject("name", "MongoDB")
+//			        .append("type", "database")
+//			        .append("count", i)
+//			        .append("info", new BasicDBObject("x", 203).append("y", 102));
+//			coll.insertOne(doc);
+//		}
+		coll.deleteMany(new Document());
 	}
 }
