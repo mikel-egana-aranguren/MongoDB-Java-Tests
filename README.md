9a2a19d667c73924d22e148b002b2777b39242fa
mikel <mikel@mikel-Precision-Tower-3620>
2018-10-19 09:17:19 +0200

    75000 docs and CSV export
    Added 75000 documents, queried, and exported to CSV, all very fast.
    
    Added exec and CSV export instructions to README.

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # MongoDB-Java-Tests
 Just playing around with MongoDB and Java: http://mongodb.github.io/mongo-java-driver/
+
+To start Mongo (I don't why it is not started automatically) and the Web client for Mongo:
+
+```
+service mongod start
+mongodb-compass-community
+```
+
+Export CSV:
+
+```
+mongoexport --db JavaTests --collection BigCollection --type=csv --fields name,type,count,info.x,info.y --out ./JavaTestsBigCollection.csv
+```
--- src/main/java/org/biocrucesbizkaia/bioinformatics/mongodb/Exec.java
+++ src/main/java/org/biocrucesbizkaia/bioinformatics/mongodb/Exec.java
@@ -1,6 +1,8 @@
 package org.biocrucesbizkaia.bioinformatics.mongodb;
 
+import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
+import com.mongodb.DBCollection;
 import com.mongodb.MongoClient;
 import java.net.UnknownHostException;
 
@@ -10,7 +12,21 @@ public class Exec {
 		try {
 			MongoClient mongoClient = new MongoClient();
 			DB db = mongoClient.getDB( "JavaTests" );
-			System.out.println(db.collectionExists("JavaCollection"));
+//			System.out.println("JavaCollection exists: " + db.collectionExists("JavaCollection"));
+//			DBCollection coll = db.getCollection("JavaCollection");
+			DBCollection coll = db.getCollection("BigCollection");
+			
+			int i = 0;
+			
+			while (i < 75000) {
+				i+=1;
+				System.out.println(i);
+				BasicDBObject doc = new BasicDBObject("name", "MongoDB")
+				        .append("type", "database")
+				        .append("count", i)
+				        .append("info", new BasicDBObject("x", 203).append("y", 102));
+				coll.insert(doc);
+			}
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		}
